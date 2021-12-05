cedc853854473a40dbda012ffeae2512b73888f6
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-12-05 13:35:46 -0700

    added footer and logos

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,1 +1,34 @@
-// each project featured should include: img, title, link to deployed app and link to github repo
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div>
+        <a
+          href="https://github.com/aeforbush"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={require("../../assets/logos/Octocat.jpg")}
+            alt="Github"
+            className="logo"
+          ></img>
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://www.linkedin.com/in/aforbush/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={require("../../assets/logos/LI-In-Bug.png")}
+            alt="LinkedIn"
+            className="logo"
+          ></img>
+        </a>
+      </div>
+    </footer>
+  );
+}
